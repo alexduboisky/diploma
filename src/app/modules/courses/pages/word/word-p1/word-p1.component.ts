6f735bcb0b187589f7962c53d2574237e5b3763e
Alexander Duboysky <alexander.duboysky@silkdata.ai>
2023-04-27 19:06:09 +0300

    add practice for word course

--- src/app/modules/courses/pages/word/word-p1/word-p1.component.ts
+++ src/app/modules/courses/pages/word/word-p1/word-p1.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
+import {DatabaseService} from "../../../../../services/database.service";
+import {AuthService} from "../../../../../services/auth.service";
 
 @Component({
   selector: 'app-word-p1',
@@ -7,4 +10,15 @@ import { Component } from '@angular/core';
 })
 export class WordP1Component {
 
+  constructor(private router: Router, private db: DatabaseService, private auth: AuthService) {
+  }
+  prevLesson() {
+    this.db.updateCourseToUser(this.auth.User.getValue().id, 'c3', 'l1')
+    this.router.navigate(['courses/c4/l1'])
+  }
+
+  openTest() {
+    this.db.updateCourseToUser(this.auth.User.getValue().id, 'c3', 't1')
+    this.router.navigate(['courses/c4/t1'])
+  }
 }
